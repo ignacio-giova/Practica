294a4bfb344faea0393967d4ed7873412b03ff87
igna <ignacio.giovannetti@gmail.com>
2024-09-09 11:31:27 -0300

    ej 4

--- Optimizacion/diccionarios.hs
+++ Optimizacion/diccionarios.hs
@@ -1,4 +1,4 @@
-data Contacto = CDatos String String String String ...
+data Contacto = CDatos String String String String
 type Tel = Int
 type Agenda = [(Tel, Contacto)]
 
@@ -9,10 +9,19 @@ buscarContacto telefono ((t, c):agenda)
   | otherwise = buscarContacto telefono agenda
 
 agregarContacto :: Tel -> Contacto -> Agenda -> Agenda
-agregarContacto t c agenda = (t, c):: agenda
+agregarContacto t c agenda = (t, c): agenda
 
 borrarContacto :: Tel -> Agenda -> Agenda
 borrarContacto telefono [] = []
 borrarContacto telefono ((t, c): agenda)
   |telefono == t = agenda
   |otherwise = (t, c) : (borrarContacto telefono agenda)
+
+acumulacionClaves:: [(a,b)] -> [(a, [b])]
+acumulacionClaves [] = []
+acumulacionClaves ((k, v):xs) = go k v xs []
+  where
+    go k v [] resultado = (k ,[v]) : resultado
+    go k v ((k', v'):xs) resultado
+      |k == k' = go k v xs ((k', v':v) : resultado)
+      |otherwise = go k v xs ((k', [k']) : resultado)
