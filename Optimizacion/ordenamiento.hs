d27e3da2b11180a927356e59c20d618537521c72
Ignacio <Ignacio.giovannetti@gmail.com>
2024-09-13 11:46:44 -0300

    Ej5

--- Optimizacion/ordenamiento.hs
+++ Optimizacion/ordenamiento.hs
@@ -1,4 +1,6 @@
 import Data.List
+import Data.Map
+import Prelude hiding (lookup)
 
 ordenar :: Ord a => [a] -> [a]
 ordenar = Data.List.sort
@@ -30,3 +32,42 @@ ruedaFortuna xs p = go (ordenar xs) p 0
           |((head xs) + (last xs)) > p = go (init xs) p (resultado + 1)
           |otherwise = go (tail(init xs)) p (resultado + 1)
 
+instance Ord Accion where
+  compare (Entrada t1) (Entrada t2) = compare t1 t2
+  compare (Salida t1) (Salida t2) = compare t1 t2
+  compare (Entrada t1) (Salida t2) = compare t1 t2
+  compare (Salida t1) (Entrada t2) = compare t1 t2
+
+data Accion = Entrada Int | Salida Int deriving (Show, Eq)
+
+maxComensales :: [(Int, Int)] -> Int
+maxComensales comensales = 
+  let acciones = aplanarAcciones comensales in
+  maximum (go (ordenar acciones) 0)
+  where 
+    aplanarAcciones :: [(Int, Int)] -> [Accion]
+    aplanarAcciones = foldr (\(e, s) r -> Entrada e : Salida s : r) []
+
+    go [] actuales = [actuales]
+    go ((Entrada _): xs) actuales = actuales : go xs (actuales + 1)
+    go ((Salida _): xs) actuales = actuales : go xs (actuales - 1)
+
+dosValores :: [Int] -> Int -> Maybe (Int, Int)
+dosValores lista n = go lista (acumular lista empty)
+  where
+    acumular :: [Int] -> Map Int Int -> Map Int
+    acumular [] r = r
+    acumular (x:xs) map=
+      case lookup x map of
+        Nothing -> acumular xs (insert x 1 map)
+        Just c -> acumular xs (insert x (c + 1) (delete c map))
+    
+    go [] _ = Nothing
+    go (x:xs) map =
+      let comp = n - x 
+      case lookup comp map of
+        Nothing -> go xs map
+        Just veces ->
+          if comp == n && veces == 1
+            then go xs map
+            else (x, comp)
